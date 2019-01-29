94951d272ba1029a5f189c4b046a404f25f48d71
Dimitri Pandeleakis <42065117+DimPaDev@users.noreply.github.com>
2019-01-29 19:45:20 +0100

    Init project

---
+++ install.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const { crxDownload } = require('chrome-extension-downloader');
+const unzip = require("unzip-crx");
+const vuejsDevToolsId = 'nhdogjmejiglipccpnnnanhbledajbpd';
+const crx = path.join(__dirname, vuejsDevToolsId + '.crx');
+const manifestFile = path.join(__dirname, 'vue/manifest.json');
+const outDir = path.join(__dirname, 'vue');
+
+crxDownload(vuejsDevToolsId).then(buffer => {
+	console.log(' - Found Vue.js DevTools Crx Extension');
+	fs.writeFile(crx, buffer, err => {
+	  if (err) throw err;
+		console.log(' - Saved Crx File!');
+		unzip(crx, outDir).then(() => {
+			console.log(" - Successfully unzipped the crx file..");
+			fs.unlinkSync(crx);
+			console.log(" - Reading Manifest file ...");
+			fs.readFile(manifestFile, 'utf8', (err, data) => {
+				if (err) return console.log(err);
+				const toBeReplaced = `"file:///*",`;
+				const replacement = `"file:///*","*://*/*",`;
+				const result = data.replace(toBeReplaced, replacement);
+				console.log(" - Patching Vue DevTools manifest file...");
+				fs.writeFile(manifestFile, result, 'utf8', (err) => {
+					if (err) return console.log(err);
+					console.log('[OK] Done');
+				});
+			});
+		});
+	});
+});
