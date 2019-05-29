9cfb82c30de63ec183dba53462222d893c6fa37f
Dimitri Pandeleakis <dimitris88@gmail.com>
2019-05-29 15:21:15 +0200

    migrated to updated deps

--- install.js
+++ install.js
@@ -1,32 +1,28 @@
 const fs = require('fs');
 const path = require('path');
-const { crxDownload } = require('chrome-extension-downloader');
-const unzip = require("unzip-crx");
+const unzip = require("unzip-crx-3");
 const vuejsDevToolsId = 'nhdogjmejiglipccpnnnanhbledajbpd';
 const crx = path.join(__dirname, vuejsDevToolsId + '.crx');
 const manifestFile = path.join(__dirname, 'extension/manifest.json');
 const outDir = path.join(__dirname, 'extension');
+const downloadCRX = require('download-crx');
 
-crxDownload(vuejsDevToolsId).then(buffer => {
-	// console.log(' - Found Vue.js DevTools Crx Extension');
-	fs.writeFile(crx, buffer, err => {
-	  if (err) throw err;
-		// console.log(' - Saved Crx File!');
-		unzip(crx, outDir).then(() => {
-			// console.log(" - Successfully unzipped the crx file..");
-			fs.unlinkSync(crx);
-			// console.log(" - Reading Manifest file ...");
-			fs.readFile(manifestFile, 'utf8', (err, data) => {
-				if (err) return console.log(err);
-				const toBeReplaced = `"file:///*",`;
-				const replacement = `"file:///*","*://*/*",`;
-				const result = data.replace(toBeReplaced, replacement);
-				// console.log(" - Patching Vue DevTools manifest file...");
-				fs.writeFile(manifestFile, result, 'utf8', (err) => {
-					if (err) return console.log(err);
-					console.log('[OK] Vue DevTools Extension successfully installed!');
-				});
-			});
-		});
-	});
+downloadCRX.downloadById(vuejsDevToolsId, __dirname, vuejsDevToolsId).then(filePath => { 
+  // console.log(`crx is located in ${filePath}`); 
+  unzip(filePath, outDir).then(() => {
+    // console.log(" - Successfully unzipped the crx file..");
+    fs.unlinkSync(crx);
+    // console.log(" - Reading Manifest file ...");
+    fs.readFile(manifestFile, 'utf8', (err, data) => {
+      if (err) return console.log(err);
+      const toBeReplaced = `"file:///*",`;
+      const replacement = `"file:///*","*://*/*",`;
+      const result = data.replace(toBeReplaced, replacement);
+      // console.log(" - Patching Vue DevTools manifest file...");
+      fs.writeFile(manifestFile, result, 'utf8', (err) => {
+        if (err) return console.log(err);
+        console.log('[OK] Vue DevTools Extension successfully installed!');
+      });
+    });
+  });
 });
